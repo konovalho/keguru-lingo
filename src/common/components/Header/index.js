450b2668123d53cbe8ccc8af029128789a605c0b
ArtemiusL <lamber95@icloud.com>
2021-03-12 02:10:56 +0300

    Remove unused dependecies

--- src/common/components/Header/index.js
+++ src/common/components/Header/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles'
 import { css } from '@linaria/core';
 import { media, hoverSupport, colors } from '@styles/utils';
