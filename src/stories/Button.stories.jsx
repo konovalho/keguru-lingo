4ca140e2e8e745a09871030eecf5f9bea5915737
ArtemiusL <lamber95@icloud.com>
2021-03-12 12:38:10 +0300

    add class to storybook

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Button from '../common/components/Button';
+import { css } from '@linaria/core';
 
 export default {
   title: 'Button',
   component: Button,
 };
+
+const root = css`
+  background-color: green;
+  padding: '10px'
+`;
 
 const Template = ({ children, ...args }) => (
   <Button {...args}>{children}</Button>
@@ -14,5 +20,6 @@ export const Default = Template.bind({});
 Default.args = {
   children: 'Almost before we knew it, we had left the ground.',
   variant: 'metaTitle',
-  tag: 'h1'
+  tag: 'h1',
+  className: root,
 };
